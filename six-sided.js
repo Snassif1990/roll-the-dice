5d686cd97fbebd2c730cc374e3c83178ad156720
Sam Nassif <98352078+Snassif1990@users.noreply.github.com>
2022-02-09 13:38:16 -0500

    Add files via upload

--- six-sided.js
+++ six-sided.js
@@ -1,1 +1,10 @@
-console.log(Math.ceil(Math.random()*6))
+const prompt = require('prompt-sync')();
+
+const word = prompt("Would you like to roll the die? (Yes or No?) ")
+
+if(word.includes("Yes")) {
+    console.log(Math.ceil(Math.random()*6))
+}
+if(word.includes("No")) {
+    console.log("Well you're boring. ")
+}
